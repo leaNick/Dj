4589e62908ab40db09d3213eedac06ce66316a5d
leaNick <ick23211@gmail.com>
2015-12-15 00:24:12 +0200

    Night commit

--- Dj/settings.py
+++ Dj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, './templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
